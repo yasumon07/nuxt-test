6c6b428750e43e14c72e510e0eb48450413f4049
yasumoto <yasumoto@sra-tohoku.co.jp>
2021-05-31 01:09:41 +0900

    test 0109

--- pages/index.vue
+++ pages/index.vue
@@ -3,26 +3,8 @@
     <div>
       <Logo />
       <h1 class="title">
-        Hello, SRA.
+        Hello, SRA Tohoku.
       </h1>
-      <!-- <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div> -->
     </div>
   </div>
 </template>
